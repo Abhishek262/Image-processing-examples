2169d3d36cbba2f948642bf2ed66a35f5e9b566d
adhu <adhwaith2017.menon@gmail.com>
2019-11-01 09:48:58 +0530

    Video Processing

---
+++ main_task0_video_processing.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import os
+
+def partA():
+    vid = cv2.VideoCapture('../Videos/RoseBloom.mp4')
+    if (vid.isOpened() == FALSE):
+        print("Incorrect path name or no video")
+    suc,img = vid.read();
+    frame_count = 0
+    while suc:
+        if (frame_count == 5) :
+            cv2.imwrite('../Generated/frame_as_6.jpg',img)
+        suc,img = vid.read()
+        frame_count += 1
+    pass
+
+def partB():
+    img = cv2.imread('../Generated/frame_as_6.jpg',cv2.IMREAD_UNCHANGED)
+    red = img[:,:,2]
+    red_img = np.zeros(img.shape)
+    red_img[:,:,2] = red;
+    cv2.imwrite('../Generated/frame_as_6_red.jpg',red_img)
+    pass
+
+partA()
+partB()
